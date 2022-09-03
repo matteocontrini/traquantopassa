e1150705063d5b300d7d1ddb4a573e4edb10440e
Matteo Contrini <matteo@contrini.it>
2022-09-03 13:56:37 +0200

    Add footer

--- pages/[stop].vue
+++ pages/[stop].vue
@@ -30,7 +30,7 @@ setInterval(() => {
 <template>
     <div class="max-w-[600px] mx-auto mt-10 px-5" v-if="data">
         <h1 class="font-semibold text-center text-4xl">{{ data.stopName }}</h1>
-        <div class="text-sm text-center">
+        <div class="text-sm flex justify-center items-center">
             aggiornato alle
             {{
                 new Date(data.lastUpdatedAt).toLocaleTimeString([], {
@@ -38,6 +38,7 @@ setInterval(() => {
                     minute: '2-digit',
                 })
             }}
+            <span class="rounded-full w-2 h-2 animate-pulse bg-green-500 ml-2 mt-0.5"></span>
         </div>
 
         <div v-for="direction in data.directions" class="mt-10">
@@ -46,6 +47,13 @@ setInterval(() => {
             </div>
             <Trip v-for="trip in direction.trips" :trip="trip" :key="trip.tripId" />
         </div>
+
+        <div class="my-10 text-neutral-500 text-sm text-center">
+            <span class="inline-block rounded-full w-2 h-2 motion-safe:animate-ping bg-green-500 mr-2 mt-0.5"></span>
+            Dati in tempo reale
+
+            <br />
+            <a href="mailto:ciao@traquantopassa.in" class="block mt-2 underline">ciao@traquantopassa.in</a>
         </div>
     </div>
 </template>
