9658cd255dd20b8792698449657804c8d9b4f16e
Matteo Contrini <matteo@contrini.it>
2022-10-03 10:19:47 +0200

    Update favicons

--- app.vue
+++ app.vue
@@ -18,8 +18,8 @@ import '@fontsource/inter/variable.css';
                 body="true"
             />
             <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+            <Link rel="icon" type="image/png" sizes="192x192" href="/favicon-192.png" />
         </Head>
 
         <NuxtPage />
