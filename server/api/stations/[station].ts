a8a64ea97a3a6320744c9447de5a5f4a694a856c
Matteo Contrini <matteo@contrini.it>
2022-10-28 14:42:21 +0200

    Improve capitalization when there are dots

--- server/api/stations/[station].ts
+++ server/api/stations/[station].ts
@@ -122,6 +122,7 @@ function parseTrains(html: string): Train[] {
 function capitalize(str: string): string {
     return str
         .toLowerCase()
+        .replace(/\.(\w)/g, '. $1') // e.g. "VENEZIA S.LUCIA" -> "VENEZIA S. LUCIA"
         .split(' ')
         .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
         .join(' ');
