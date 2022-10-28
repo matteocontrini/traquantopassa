f255ca7a8c312f924f1186d5bb9c2a28f07902bd
Matteo Contrini <matteo@contrini.it>
2022-10-28 23:44:42 +0200

    Update readme and info page

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Tra quanto passa (l'autobus)
+# Tra quanto passa
 
-*Tra quanto passa* is a simple web application that shows the arrival times of buses in the city of Trento.
+*Tra quanto passa* is a simple web application that shows the arrival times of **buses** in the city of Trento and **trains** information for some popular stations in Northeast Italy.
 
-It also takes into consideration delays based on real-time data.
+It also takes into consideration delays based on real-time data, for both buses and trains.
 
 It's currently hosted on [traquantopassa.in](https://traquantopassa.in).
 
--- pages/info.vue
+++ pages/info.vue
@@ -7,39 +7,50 @@ useHead({
 </script>
 
 <template>
-    <main>
-        <p>
-            I dati degli autobus mostrati sul sito sono forniti da Trentino Trasporti. Se perdi l'autobus non è colpa
-            mia. Se mancano i dati in tempo reale non è colpa mia.
-        </p>
+    <div>
+        <header>
+            <h1 class="font-semibold text-4xl">Tra quanto passa</h1>
+        </header>
 
-        <p>
-            I dati sui treni sono forniti da Rete Ferroviaria Italiana (RFI) e sono gli stessi mostrati nelle stazioni
-            ferroviarie. Se sono sbagliati non è colpa mia.
-        </p>
+        <main class="mt-8">
+            <p>
+                I dati degli autobus mostrati sul sito sono forniti da Trentino Trasporti. Se perdi l'autobus non è
+                colpa mia. Se mancano i dati in tempo reale non è colpa mia.
+            </p>
 
-        <p>
-            Il progetto non è comunque in alcun modo affiliato con Trentino Trasporti, con la Provincia Autonoma di
-            Trento o con RFI/Ferrovie dello Stato.
-        </p>
+            <p>
+                I dati sui treni sono forniti da Rete Ferroviaria Italiana (RFI) e sono gli stessi mostrati nelle
+                stazioni ferroviarie. Se sono sbagliati non è colpa mia.
+            </p>
 
-        <p>
-            La posizione geografica opzionalmente raccolta nelle pagine con la lista delle fermate/stazioni non viene
-            mai inviata al server. Viene utilizzata esclusivamente sul tuo dispositivo per calcolare la distanza dalle
-            fermate/stazioni.
-        </p>
+            <p>
+                Il progetto non è comunque in alcun modo affiliato con Trentino Trasporti, con la Provincia Autonoma di
+                Trento o con RFI/Ferrovie dello Stato.
+            </p>
 
-        <p>
-            Per contattare l'autore del sito:
-            <a href="mailto:ciao@traquantopassa.in">ciao@traquantopassa.in</a>
-        </p>
+            <p>
+                La posizione geografica opzionalmente raccolta nelle pagine con la lista delle fermate/stazioni non
+                viene mai inviata al server. Viene utilizzata esclusivamente sul tuo dispositivo per calcolare la
+                distanza dalle fermate/stazioni.
+            </p>
 
-        <p class="mt-8">
-            <NuxtLink to="/">Vai alla lista delle fermate (autobus)</NuxtLink>
-        </p>
+            <p>
+                Per contattare l'autore del sito:
+                <a href="mailto:ciao@traquantopassa.in">ciao@traquantopassa.in</a>
+            </p>
 
-        <p class="mt-4">
-            <NuxtLink to="/treni">Vai alla lista delle stazioni (treni)</NuxtLink>
-        </p>
-    </main>
+            <p>
+                Il codice sorgente è pubblicato
+                <a target="_blank" href="https://github.com/matteocontrini/traquantopassa">su GitHub</a>.
+            </p>
+
+            <p class="mt-12">
+                <NuxtLink to="/">Vai alla lista delle fermate (autobus)</NuxtLink>
+            </p>
+
+            <p class="mt-4">
+                <NuxtLink to="/treni">Vai alla lista delle stazioni (treni)</NuxtLink>
+            </p>
+        </main>
+    </div>
 </template>
