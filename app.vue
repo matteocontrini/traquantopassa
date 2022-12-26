b34a243762ccb25cfc1fb433a5209a6ad0dd3ac1
Matteo Contrini <matteo@contrini.it>
2022-12-26 11:48:46 +0100

    Improve head initialization
    `Script` was deprecated, I think

--- app.vue
+++ app.vue
@@ -1,27 +1,32 @@
 <script setup>
 import '@fontsource/inter/variable.css';
+import { useHead } from '#head';
+
+useHead({
+    title: 'Tra quanto passa',
+    meta: [{ name: 'theme-color', content: '#171717' }],
+    link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon-192.png' },
+    ],
+    script: [
+        {
+            children: "if (window.location.host !== 'traquantopassa.in') window.goatcounter = { no_onload: true };",
+            tagPosition: 'bodyClose',
+        },
+        {
+            src: 'https://gc.zgo.at/count.js',
+            defer: true,
+            tagPosition: 'bodyClose',
+            'data-goatcounter': 'https://traquanto.goatcounter.com/count',
+        },
+    ],
+});
 </script>
 
 <template>
     <div class="max-w-[600px] mx-auto mt-10 px-5">
-        <Head>
-            <Title>Tra quanto passa</Title>
-            <Meta name="theme-color" content="#171717" />
-            <Script
-                children="if (window.location.host !== 'traquantopassa.in') window.goatcounter = { no_onload: true };"
-                :body="true"
-            />
-            <Script
-                data-goatcounter="https://traquantopassa.goatcounter.com/count"
-                src="https://gc.zgo.at/count.js"
-                defer
-                :body="true"
-            />
-            <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
-            <Link rel="icon" type="image/png" sizes="192x192" href="/favicon-192.png" />
-        </Head>
-
         <NuxtPage />
     </div>
 </template>
