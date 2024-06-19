33deb4d745317ac1646f6dd169c7ee581fdc505b
Matteo Contrini <matteo@contrini.it>
2024-06-19 15:05:57 +0200

    [svelte] Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import FooterNavigation from '$lib/components/FooterNavigation.svelte';
+</script>
+
+<div class="text-center mt-12">
+	<div class="text-red-500">
+		{#if $page.status === 404}
+			<p>Pagina non trovata 😕</p>
+			<p>
+				Se pensi sia un errore, contattaci
+				<a href="mailto:ciao@traquantopassa.in">via email</a>.
+			</p>
+		{:else if $page.status === 503}
+			<p>I dati di Trentino Trasporti / RFI non sono al momento disponibili 😕</p>
+			<p>Prova a ricaricare la pagina.</p>
+			<p>
+				Se il problema persiste, contattaci
+				<a href="mailto:ciao@traquantopassa.in">via email</a>.
+			</p>
+		{:else}
+			<p>Si è verificato un errore 😕</p>
+			<p>Prova a ricaricare la pagina.</p>
+			<p>
+				Se il problema persiste, contattaci
+				<a href="mailto:ciao@traquantopassa.in">via email</a>.
+			</p>
+		{/if}
+	</div>
+
+	<FooterNavigation className="my-12" />
+</div>
+
