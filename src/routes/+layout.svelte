c404ca7996ec743fcf09423658bbd3e94f552f39
Matteo Contrini <matteo@contrini.it>
2024-06-19 10:11:12 +0200

    [svelte] Add loading bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
-<script>
+<script lang="ts">
 	import '@fontsource-variable/inter';
 	import '../app.css';
+	import { navigating } from '$app/stores';
+	import { onMount } from 'svelte';
+	import type { Topbar } from 'topbar';
+
+	let topbar: Topbar;
+	onMount(async () => {
+		topbar = await import('topbar') as unknown as Topbar;
+		topbar.config({
+			barColors: {
+				'0': '#ffffff'
+			}
+		});
+	});
+
+	$: {
+		if ($navigating) {
+			topbar?.show();
+		} else {
+			topbar?.hide();
+		}
+	}
 </script>
 
 <div class="max-w-[600px] mx-auto mt-10 px-5">
