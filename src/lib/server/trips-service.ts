21bfdc42bc43430cdd193d39fc56e202af43dff7
Matteo Contrini <matteo@contrini.it>
2024-06-19 12:00:36 +0200

    [svelte] Port the rest of the trips algorithm

--- src/lib/server/trips-service.ts
+++ src/lib/server/trips-service.ts
@@ -10,6 +10,7 @@ const cache = new NodeCache();
 
 const tripsCacheDurationSeconds = 30;
 const defaultLimit = 15;
+const outdatedDataThresholdMillis = 1000 * 60 * 5;
 
 export async function getTrips(stop: Stop) {
 	const stopId = stop.id;
@@ -23,11 +24,10 @@ export async function getTrips(stop: Stop) {
 
 	// Fetch from API
 	console.log('Fetching trips for stop', stopId);
-	// TODO: limit
 	const apiTrips = await api.getTrips(stopId, defaultLimit);
 	const routes = await routesService.getRoutes();
 
-	const trips = await mapApiTrips(apiTrips, routes);
+	const trips = await mapApiTrips(apiTrips, routes, stopId);
 
 	direction = {
 		name: directionName(stop),
@@ -40,18 +40,68 @@ export async function getTrips(stop: Stop) {
 	return direction;
 }
 
-async function mapApiTrips(apiTrips: api.ApiTrip[], routes: Route[]) {
+async function mapApiTrips(apiTrips: api.ApiTrip[], routes: Route[], userStopId: number) {
 	return apiTrips.map(trip => {
 		const route = routes.find(r => r.id === trip.routeId)!;
+
+		// Compute wait time in minutes
+		const expectedTime = new Date(trip.oraArrivoEffettivaAFermataSelezionata);
+		let minutes = Math.ceil((expectedTime.getTime() - Date.now()) / 1000 / 60);
+		if (minutes < 0) {
+			minutes = 0;
+		}
+
+		// Check if the trip is at the last stop and don't use the delay in that case
+		// (the delay is always 0 in that case)
+		// Note: use stopNext because sometimes the last stop is never reached
+		const endOfRouteStopId = trip.stopTimes.at(-1)!.stopId;
+		const nextStopId = trip.stopNext;
+		const delay = endOfRouteStopId != nextStopId ? trip['delay'] : null;
+
+		// *** Compute how many stops away the bus is
+		let distanceInStops = null;
+		const currentStopSequenceNumber = trip.lastSequenceDetection;
+
+		// If the bus is detected to be already at the first stop
+		if (currentStopSequenceNumber > 0) {
+			// Find the stop where the bus is
+			const currentBusStop = trip.stopTimes.find(
+				(stop) => stop.stopSequence == currentStopSequenceNumber
+			);
+			// Find the stop where the user is, but only after the current bus stop
+			const currentUserStop = trip.stopTimes.find(
+				(stop) => stop.stopId == userStopId && stop.stopSequence >= currentStopSequenceNumber
+			);
+			// If it's null, it means the bus is already beyond the user stop
+			if (currentUserStop == null) {
+				distanceInStops = -2;
+			} else {
+				// Compute how many stops away the bus is
+				distanceInStops = currentUserStop.stopSequence - currentBusStop!.stopSequence;
+			}
+		}
+			// If the bus hasn't still reached the first stop, but it's sending data,
+		// it means it's about to depart
+		else if (currentStopSequenceNumber == 0 && delay != null) {
+			distanceInStops = -1;
+		}
+
+		// Check if the last update of real-time data isn't recent enough
+		let isOutdated = false;
+		if (delay != null) {
+			const lastEventDate = new Date(trip.lastEventRecivedAt);
+			isOutdated = (Date.now() - lastEventDate.getTime()) > outdatedDataThresholdMillis;
+		}
+
 		return {
 			id: trip.tripId,
 			routeName: route.name,
 			routeColor: route.color,
 			destination: trip.tripHeadsign,
-			minutes: 0, // TODO
-			delay: trip.delay,
-			distanceInStops: null, // TODO
-			isOutdated: false // TODO
+			minutes,
+			delay,
+			distanceInStops,
+			isOutdated
 		} as Trip;
 	});
 }
