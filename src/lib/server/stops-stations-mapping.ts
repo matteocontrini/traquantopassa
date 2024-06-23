a646918afce3c091a1efcd124ecb51e11e8cc8e7
Matteo Contrini <matteo@contrini.it>
2024-06-23 15:05:32 +0200

    [svelte] Add more stops-stations mappings

--- src/lib/server/stops-stations-mapping.ts
+++ src/lib/server/stops-stations-mapping.ts
@@ -1,7 +1,10 @@
 // Mappings from stop slug to station slug
 const mapping = {
 	'stazione': 'trentofs',
-	'mesianofs': 'povomesiano'
+	'mesianofs': 'povomesiano',
+	'25650': 'villazzano',
+	'questura': 'sanbartolameo',
+	'21010': 'santachiara',
 } as Record<string, string>;
 
 export function getStationForStop(stopSlug: string): string | null {
