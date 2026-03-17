ed7f8fb59564cd4cc0a2905fa18b8c4e9b88f10c
Alberto Gusmeroli <96133902+guss-alberto@users.noreply.github.com>
2026-03-17 20:04:31 +0100

    add ftm routes to sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,5 +1,6 @@
 import * as stopsService from '$lib/server/stops-service';
 import * as stationsService from '$lib/server/stations-service';
+import ftmStationsList from '$lib/server/ftm/ftm-stations-list';
 
 const BASE_URL = 'https://traquantopassa.in';
 
@@ -8,6 +9,7 @@ export async function GET() {
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url><loc>${BASE_URL}/</loc></url>
 	<url><loc>${BASE_URL}/treni</loc></url>
+	<url><loc>${BASE_URL}/ftm</loc></url>
 	<url><loc>${BASE_URL}/info</loc></url>
 	<url><loc>${BASE_URL}/aiuto</loc></url>
 `;
@@ -22,6 +24,11 @@ export async function GET() {
 	const stations = stationsService.getStationList();
 	for (const station of stations) {
 		xml += `	<url><loc>${BASE_URL}/treni/${station.slug}</loc></url>
+`;
+	}
+
+	for (const station of ftmStationsList) {
+		xml += `	<url><loc>${BASE_URL}/ftm/${station.slug}</loc></url>
 `;
 	}
 
