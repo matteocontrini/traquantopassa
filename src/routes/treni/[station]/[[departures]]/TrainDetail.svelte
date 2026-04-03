1f8b67dbea42f2ae38cbb2bcf057f7c6c9272978
Matteo Contrini <matteo@contrini.it>
2026-04-03 23:45:12 +0200

    Improve code formatting and no info rendering

--- src/routes/treni/[station]/[[departures]]/TrainDetail.svelte
+++ src/routes/treni/[station]/[[departures]]/TrainDetail.svelte
@@ -13,33 +13,35 @@
 <div class="pt-1 pb-3">
 	<div class="rounded-lg bg-neutral-800 border border-neutral-700">
 		<div class="max-h-40 py-3 overflow-y-auto px-4 flex flex-col gap-y-2.5">
-		{#if train.notes}
-			<div class="font-bold text-xl">
-				Informazioni:
-			</div>
-			{train.notes}
-		{/if}
-		{#if train.stopTimes.length}
-			<div class="font-bold text-xl">
-				Ferma a:
-			</div>
-			{#each train.stopTimes as stopTime}
-				<div class="flex items-center gap-x-4">
-					<div class="w-10 leading-none font-semibold">
-						{stopTime.time}
-					</div>
-
-					<div class="whitespace-nowrap leading-none">
-						{stopTime.name}
-					</div>
+			{#if train.notes}
+				<div class="font-bold text-xl">
+					Informazioni:
 				</div>
-			{/each}
-		{/if}
-		{#if !train.notes && !train.stopTimes.length}
-			<div class="italic text-center text-neutral-300">
-				Nessuna informazione disponibile per questo {train.category == 'Bus' || train.isReplacedByBus ? 'autobus' : 'treno'}
-			</div>
-		{/if}
+				{train.notes}
+			{/if}
+			{#if train.stopTimes.length}
+				<div class="font-bold text-xl">
+					Ferma a:
+				</div>
+				<!-- eslint-disable-next-line svelte/require-each-key -->
+				{#each train.stopTimes as stopTime}
+					<div class="flex items-center gap-x-4">
+						<div class="w-10 leading-none font-semibold">
+							{stopTime.time}
+						</div>
+
+						<div class="whitespace-nowrap leading-none">
+							{stopTime.name}
+						</div>
+					</div>
+				{/each}
+			{/if}
+			{#if !train.notes && !train.stopTimes.length}
+				<div class="text-center text-neutral-300">
+					Nessuna informazione disponibile per
+					questo {train.category === 'Bus' || train.isReplacedByBus ? 'autobus' : 'treno'}
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
