f2e36493bc929f2b4c28633f1f0170f8c5a5f946
Matteo Contrini <matteo@contrini.it>
2026-05-16 10:04:35 +0200

    Fix remote IP address retrieval in hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import * as logger from '$lib/logger';
 
 export const handle: Handle = ({ event, resolve }) => {
-	const ip = event.request.headers.get('fly-client-ip') || event.getClientAddress();
+	const ip = event.request.headers.get('x-forwarded-for')?.split(',')[0] || event.getClientAddress();
 	logger.info(`${ip} - ${event.request.method} ${event.url.pathname + event.url.search}`);
 	return resolve(event);
 };
