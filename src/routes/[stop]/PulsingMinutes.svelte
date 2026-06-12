38557dbeb450f1e481e91af21375a22951da005d
Ceralex <alessandroceresa05@gmail.com>
2026-06-12 11:15:23 +0200

    Show arrival time under countdown

--- src/routes/[stop]/PulsingMinutes.svelte
+++ src/routes/[stop]/PulsingMinutes.svelte
@@ -22,6 +22,12 @@
 		}
 	}
 
+	let arrivalTime = $derived.by(() => {
+		const date = new Date();
+		date.setMinutes(date.getMinutes() + minutes);
+		return date.toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' });
+	});
+
 	function syncAnimation() {
 		el.getAnimations().forEach((a) => (a.startTime = 0));
 	}
@@ -31,13 +37,18 @@
 	});
 </script>
 
-<div
-	bind:this={el}
-	onanimationstart={syncAnimation}
-	class="text-right text-xl {dimmed ? 'font-medium' : 'font-semibold'} whitespace-nowrap"
-	class:text-neutral-500={dimmed}
-	class:animate-pulse={minutes === 0}
-	class:text-red-600={!dimmed && minutes === 0}
->
-	{formatMinutes(minutes)}
+<div class="flex flex-col items-end justify-center">
+ 	<div
+ 		bind:this={el}
+		onanimationstart={syncAnimation}
+		class="text-right text-xl {dimmed ? 'font-medium' : 'font-semibold'} whitespace-nowrap"
+		class:text-neutral-500={dimmed}
+		class:animate-pulse={minutes === 0}
+		class:text-red-600={!dimmed && minutes === 0}
+	>
+		{formatMinutes(minutes)}
+	</div>
+	<div class="text-xs text-neutral-500">
+		{arrivalTime}
+	</div>
 </div>
